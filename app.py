a03a9f923dabd5949c5b17c9675cb78d7149fee8
sanjida-777 <sanjida.777@hotmail.com>
2025-02-03 23:41:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import os, json
+import logging
 
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
 # Telegram Bot Credentials
