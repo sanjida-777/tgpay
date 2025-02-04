fb22b420aa284baa463a0d4e4a181e93468ef29a
sanjida-777 <sanjida.777@hotmail.com>
2025-02-03 23:21:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,3 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
 
-    return jsonify({"message": "Refund request recorded. Wait for Telegram to process it.", "payment_id": payment_id})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
